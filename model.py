ed9ac10db7fbf5854fe005aebafaf4fb1a07cfae
Bach <michaelbach0707@gmail.com>
2026-04-16 16:02:21 +0200

    adjusted requirements

---
+++ model.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class IrisClassificationNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(4, 32)
+        self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, 3)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = torch.relu(x)
+        x = self.fc2(x)
+        x = torch.relu(x)
+        x = self.fc3(x)
+        return x
